1f844e44547986dc8a818a420310cbc1321fa2a1
John-Dunn <ehez.services@gmail.com>
2018-06-23 22:52:10 +0200

    Added token that can serve as a ERC20 faucet

---
+++ ERC20Faucet/contracts/FaucetToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+import "./StandardToken.sol";
+
+/**
+ * @title Faucet Token
+ * @dev Simple ERC20 Token example which serves as a faucet. Allows anybody to send in ETH in exchange for the same amount of this token.
+ * Based on the MintableToken by OpenZeppelin: https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC20/MintableToken.sol
+ */
+contract FaucetToken is StandardToken {
+    event Mint(address indexed to, uint256 amount);
+
+    /**
+    * @dev Function to mint tokens.
+    * @return A boolean that indicates if the operation was successful.
+    */
+    function mint() public payable returns (bool) {
+        totalSupply_ = totalSupply_.add(msg.value);
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        emit Mint(msg.sender, msg.value);
+        emit Transfer(address(0), msg.sender, msg.value);
+        return true;
+    }
+}
---
+++ ERC20Faucet/contracts/GenericERC20Token.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+import "./FaucetToken.sol";
+
+/**
+ * @title Generic Token example that anybody can mint by calling the mint function with some ETH. The same amount of this token is then minted and assigned to the sender.
+ */
+contract GenericERC20Token is FaucetToken {
+
+    string public constant name = "GenericERC20Token";
+    string public constant symbol = "GERCT";
+    uint8 public constant decimals = 18;
+
+}
