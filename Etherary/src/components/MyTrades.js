d8fd44e1412d73d8cc5c7fc1d2746c072cb1af87
John-Dunn <ehez.services@gmail.com>
2018-08-15 09:16:27 +0200

    Component displaying all trades (WIP)

--- Etherary/src/components/MyTrades.js
+++ Etherary/src/components/MyTrades.js
@@ -1,14 +1,86 @@
 import React, { Component } from 'react'
+import { Button, Row, Col, FormGroup } from 'reactstrap';
+
+import {getContractInstance, instantiateContractAt} from '../utils/getContractInstance'
+
+import Etherary from '../../build/contracts/Etherary.json'
+import TradeCardWrapper from './TradeCards/TradeCardWrapper'
+
+
 
 class MyTrades extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trades: [],
+            done: false
+        }
+    }
+
+
+
+    getAllTrades() {
+        var EtheraryInstance = getContractInstance(Etherary, this.props.web3);
+        EtheraryInstance.orderId()
+        .then(function(numberOfTrades) {
+
+            for (var i = 0; i<numberOfTrades.toNumber(); i++) {
+                EtheraryInstance.idToSellOrder(i)
+                .then(function(trade){
+                    console.log("Trade Added", trade);
+                    this.setState(prevState => ({
+                        trades: prevState.trades.concat([trade])
+                    }))
+                }.bind(this))
+            }
+        }.bind(this))
+
+    }
+
+    displayTwoTrades() {
+        console.log("First Trade", this.state.trades[0]);
+        return(
+            <FormGroup row>
+                <Col sm="6">
+                <TradeCardWrapper
+                    web3={this.props.web3}
+                    tradeId={0}
+                    trade={this.state.trades[0]}
+                    makerTokenWithdrawable={false}
+                    takerTokenWithdrawable={false}
+                  />
+                </Col>
+
+                <Col sm="6">
+                <TradeCardWrapper
+                    web3={this.props.web3}
+                    tradeId={0}
+                    trade={this.state.trades[1]}
+                    makerTokenWithdrawable={false}
+                    takerTokenWithdrawable={false}
+                  />
+                </Col>
+            </FormGroup>
+        )
+    }
 
     render() {
+        console.log("number of trades", this.state.trades.length)
         return (
             <div>
-                <h2> You can browse your trades here!</h2>
-
+                <p>
+                    This is an experimental feature. Prior trades are called from the blockchain. It would be better for a persistent database to keep accurate track of trades listening to the events.
+                </p>
+                <div className="centered">
+                    <Button color="primary" size="lg" onClick={this.getAllTrades.bind(this)} >Load Trades</Button> {' '}
+                    <Button color="primary" size="lg" onClick={() => {this.setState({done: true})}} >Display Trades</Button>
+                </div>
+                {
+                    this.state.done
+                    ? this.displayTwoTrades()
+                    : "Well"
+                }
             </div>
-
         );
     }
 }
