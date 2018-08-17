5ecbdaf56d05b795e1ebe44d7f8dfedb23af34d5
John-Dunn <ehez.services@gmail.com>
2018-08-17 15:17:44 +0200

    two contract support

--- Etherary/src/utils/tradeUnpacking.js
+++ Etherary/src/utils/tradeUnpacking.js
@@ -1,3 +1,14 @@
+// struct Trade {
+//     //AssetType assetType;
+//     address maker;
+//     address taker;
+//     address makerTokenContract;
+//     address takerTokenContract;
+//     uint256 makerTokenId; /// @dev only used for ERC721
+//     uint256 takerTokenId; /// @dev only used for ERC721
+//     bool isActive;
+// }
+
 export function tradeToMaker (trade) {
     return trade[0];
 }
@@ -6,18 +17,22 @@ export function tradeToTaker (trade) {
     return trade[1];
 }
 
-export function tradeToContract (trade) {
+export function tradeToMakerContract (trade) {
     return trade[2];
 }
 
-export function tradeToMakerTokenId (trade) {
-    return trade[3].toNumber();
+export function tradeToTakerContract (trade) {
+    return trade[3];
 }
 
-export function tradeToTakerTokenId (trade) {
+export function tradeToMakerTokenId (trade) {
     return trade[4].toNumber();
 }
 
+export function tradeToTakerTokenId (trade) {
+    return trade[5].toNumber();
+}
+
 export function tradeToActive (trade) {
-    return trade[5];
+    return trade[6];
 }
