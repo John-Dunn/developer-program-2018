a66a9ec658ed0880b0a2b103a09646332ad18fd7
John-Dunn <ehez.services@gmail.com>
2018-08-21 22:47:39 +0200

    Comment out rinkeby deployment option (requires a file not in git)

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var accounts = require("./accounts.js");
-const rinkebyToken = "https://rinkeby.infura.io/" + accounts.infuraToken();
+//var HDWalletProvider = require("truffle-hdwallet-provider");
+//var accounts = require("./accounts.js");
+//const rinkebyToken = "https://rinkeby.infura.io/" + accounts.infuraToken();
 
 
 module.exports = {
@@ -12,12 +12,12 @@ module.exports = {
       network_id: "*" // Match any network id
     },
 
-    rinkeby: {
-        provider: function() {
-            return new HDWalletProvider(accounts.rinkebyMnemonic(), rinkebyToken)
-        },
-        network_id: 4
-    }
+    // rinkeby: {
+    //     provider: function() {
+    //         return new HDWalletProvider(accounts.rinkebyMnemonic(), rinkebyToken)
+    //     },
+    //     network_id: 4
+    // }
 
   }
 }
