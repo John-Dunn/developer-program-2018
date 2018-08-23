98e9bf69cae1d77a1cad915e58b039056e69f820
John-Dunn <ehez.services@gmail.com>
2018-08-23 17:00:41 +0200

    Add two ERC20 faucets

--- migrations/3_deploy_faucet.js
+++ migrations/3_deploy_faucet.js
@@ -1,9 +1,11 @@
 var GenericERC721TokenA = artifacts.require("./GenericERC721TokenA.sol");
 var GenericERC721TokenB = artifacts.require("./GenericERC721TokenB.sol");
-var GenericERC20Token = artifacts.require("./GenericERC20Token.sol");
+var GenericERC20TokenA = artifacts.require("./GenericERC20TokenA.sol");
+var GenericERC20TokenB = artifacts.require("./GenericERC20TokenB.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(GenericERC721TokenA);
   deployer.deploy(GenericERC721TokenB);
-  deployer.deploy(GenericERC20Token);
+  deployer.deploy(GenericERC20TokenA);
+  deployer.deploy(GenericERC20TokenB);
 };
